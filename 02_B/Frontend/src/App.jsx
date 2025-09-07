0d3397d14ec6a2727c3d46ad8c65f8400b03b3d5
Rishi Rana <rishirana012354@gmail.com>
2025-09-07 19:06:12 +0530

    Done

--- 02_B/Frontend/src/App.jsx
+++ 02_B/Frontend/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   const [jokes, setjokes] = useState([]);
 
   useEffect(()=>{
-    axios.get("http://localhost:3000/jokes").then((res)=>{
+    axios.get("/api/jokes").then((res)=>{
       setjokes(res.data);
     }).catch((err)=>{
       console.log(err);
@@ -21,13 +21,13 @@ function App() {
      <p>Jokes:{jokes.length} </p>
 
      {
-      jokes.map((joke, index)=>{
+      jokes.map((joke, index)=>(
         <div key={joke.id}>
           <h3>{joke.title}</h3>
           <p>{joke.content}</p>
         </div>
 
-      })
+      ))
      }
     </>
   )
