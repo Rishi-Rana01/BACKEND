157a986a4b6ea341cf3d5a74f25d27e57f92eca4
Rishi Rana <rishirana012354@gmail.com>
2025-09-08 21:35:01 +0530

    Todo Basic

---
+++ 03_B/Backend/models/todos/sub_todo.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const subTodoSchema =new mongoose.Schema({
+  content: {type:String, required:true},
+  isCompleted: {type:Boolean, default:false},
+  createdBy:{type: mongoose.Schema.Types.ObjectId, ref:"User"},
+}, {timestamps:true});
+
+
+const SubTodo = mongoose.model("SubTodo", subTodoSchema);
+
+export default SubTodo;
