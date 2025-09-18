c46127d9f6649dafc6f5d51b4c84b2eaf97b4242
Rishi Rana <rishirana012354@gmail.com>
2025-09-18 18:55:11 +0530

    Postman, userController

--- PROJECT/src/routes/user.routes.js
+++ PROJECT/src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.Controller";
+import { registerUser } from "../controllers/user.Controller.js";
 
 const userRouter = Router()
 
