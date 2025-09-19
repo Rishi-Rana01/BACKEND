74cb2ffdfc33052276daf51910ec5a901e76230d
Rishi Rana <rishirana012354@gmail.com>
2025-09-19 21:54:16 +0530

    Register Controller

--- PROJECT/src/controllers/user.Controller.js
+++ PROJECT/src/controllers/user.Controller.js
@@ -1,11 +1,72 @@
 import {asyncHandler} from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiErrors.js";
+import {User} from "../models/user.model.js"
+import { uploadOnCloudinary } from "../utils/cloudnary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 
 const registerUser = asyncHandler( async (req, res)=> {
-    res.status(200).json({
-        message: "ok"
-    })
+
+    const {fullName , email , username, password}=req.body
+    console.log("email: ", email);
+
+   if (
+    [fullName,email,username, password].some((field)=>
+    field?.trim()==="" ) // check any of these feil is empty if emty then its true
+   ) {
+    throw new ApiError(400, "All fields are requidred")
+   }
+
+   const existedUser = User.findOne({
+    $or:[{ username },{ email }] // using $or it chek for both - if it is exsist already in db or not 
+   })
+   if (existedUser) {
+    throw new ApiError(409, "User with email or username already registered")
+   }
+
+   const avatarLocalPath =req.files?.avatar[0]?.path;   // ?-means- optional
+   const coverImageLocalPath =req.files?.coverImage[0]?.path;
+
+   if (!avatarLocalPath) {
+    throw new ApiError(400,"Avatar File is required")
+   }
+
+  const avatar= await uploadOnCloudinary(avatarLocalPath)
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+  if (!avatar) {
+     throw new ApiError(400,"Avatar File is required")
+  }
+
+  const user=await User.create({
+    fullName,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    password,
+    username:username.toLowerCase()
+  })
+
+  const createdUser=await User.findById(user._id).select("-password -refreshToken")
+  if (!createdUser) {
+    throw new ApiError(500,"Something went wrong  while Registering the user");   
+  }
+
+  return res.status(201).json(
+    new ApiResponse(200, createdUser,"User Rejistered Successfully")
+  )
+   
 } )
 
 
 export {registerUser};
+
+//GET user details from frontend
+//validation- not empty
+//user already exsist
+//check for images, chek for avatar
+//upload them to cloudianry,avatar
+//create user object - create entry in db
+//remove password and refresh tocken field from response
+//check for user creation
+//return response, res
--- PROJECT/src/models/user.model.js
+++ PROJECT/src/models/user.model.js
@@ -42,7 +42,7 @@ const userSchema = new Schema({
         type: String,
         required: [true, 'Password is reequired']
     },
-    refreshTocken:{
+    refreshToken:{
         type:String
     }
 },{
@@ -51,7 +51,7 @@ const userSchema = new Schema({
 
 userSchema.pre("save", async function(next){
     if(!this.isModified("password")) return next();
-    this.password = bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 userSchema.methods.isPasswordCorrect= async function (password) {
@@ -84,3 +84,4 @@ userSchema.methods.generateRefreshToken = function(){
 
 
 export const User = mongoose.model("User", userSchema)
+//User can contact database directly- because moongose create thhis model
